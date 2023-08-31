8de93f3f7f4bbe1112e81803aea38673a362b0b8
AtaUlMohsin <ataulmohsin900@gmail.com>
2023-08-31 16:27:20 +0500

    First commit

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains : ["res.cloudinary.com"]
+},
+  
+  experimental: {
+    serverActions: true,
+  },
+};
+
+module.exports = nextConfig;
